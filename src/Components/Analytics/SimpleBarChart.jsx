50122cb2f7178a48b00423481c0c9dbdd177b41f
Haris <harishaseeb934@gmail.com>
2026-09-18 23:09:25 +0500

    Date Ki yakki dimag kharab ke dia BC

--- src/Components/Analytics/SimpleBarChart.jsx
+++ src/Components/Analytics/SimpleBarChart.jsx
@@ -1,15 +1,7 @@
 import { BarChart, Bar, Line, XAxis, YAxis, Tooltip, ResponsiveContainer } from "recharts";
 
 const CashFlowChart = ({ data }) => (
-  <ResponsiveContainer width="100%" height={300}>
-    <BarChart data={data} margin={{ top: 10, right: 10, left: -20, bottom: 0 }}>
-      <XAxis dataKey="label" stroke="#888" tick={{ fontSize: 10, fill: '#888888' }}/>
-      <YAxis stroke="#888" tickFormatter={(v) => `$${v / 1000}k`} tick={{ fontSize: 10, fill: '#888888' }}/>
-      <Tooltip />
-      <Bar dataKey="inflow" fill="#44c08c" radius={[4, 4, 0, 0]} name="Inflow" />
-      <Bar dataKey="outflow" fill="#ac787c" radius={[4, 4, 0, 0]} name="Outflow" />
-    </BarChart>
-  </ResponsiveContainer>
+
 );
 
 export default CashFlowChart
